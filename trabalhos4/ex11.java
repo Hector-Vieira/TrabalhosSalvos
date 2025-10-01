2551166ea5372c339d0907efdda4febf61fbfceb
Hector-Vieira <hectorvieeira@gmail.com>
2025-09-30 22:24:50 -0300

    Add files via upload

---
+++ trabalhos4/ex11.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class ex11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite qual exercício você deseja consultar (11, 12, 13, 14 ou 15): ");
+        int exercicio = sc.nextInt();
+        switch (exercicio) {
+            case 11:
+                break;
+            case 12:
+                break;
+            case 13:
+                break;
+            case 14:
+                break;
+            case 15:
+                default:;
+                break;
+        }
+    }
+    public static double
+}
